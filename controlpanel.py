3b87027c409de85b81ad777a95490b19c02419d7
Mormito <guimormis@gmail.com>
2025-09-01 13:03:19 -0300

    Fix

--- controlpanel.py
+++ controlpanel.py
@@ -32,7 +32,7 @@ Choose wisely: ''').strip())
         results(target, port, num, numwor, "SYN")
     elif(opcao == 2):
         clear()
-        target = "10.0.2.5" #input("Target (IP or url) >> ").strip()
+        target = "8.8.8.8" #input("Target (IP or url) >> ").strip()
         port = 8080 #int(input("Target port >> "))
         num = int(input("Number of threads >> "))
         numwor = int(input("Number of workens (default 200) >> "))
