073920cb20646148963457e845b3eb84651587a4
Mormito <guimormis@gmail.com>
2025-09-02 13:16:49 -0300

    Creating the HTTP Request

--- http.py
+++ http.py
@@ -1,26 +1,51 @@
-from scapy.all import IP, TCP, Raw, send, sr1, RandShort
-import threading
+from scapy.all import IP, TCP, Raw, sr1, RandShort, RandIP
 
+#def send_http_packet(destiny, destiny_port):
 
-destiny = "127.0.0.1"
-destiny_port = 80
+destiny = "hitt.com.br" ##
+destiny_port = 80 ##
 
 HTTP_REQUEST = (
     b"GET / HTTP/1.1\r\n"
-    b"Host: " + destiny.encode() + b"\r\n"    # O 'Host' é importante para servidores virtuais
-    b"User-Agent: Scapy HTTP Client\r\n"      # Identificação do cliente
-    b"Connection: close\r\n"                  # Fechar a conexão após a resposta
-    b"\r\n"                                   # Linha vazia final para encerrar o cabeçalho
+    b"Host: " + destiny.encode() + b"\r\n"
+    b"User-Agent: Scapy-Client/1.0\r\n"
+    b"Connection: close\r\n"
+    b"\r\n"
 )
 
 ip_packet = IP(dst=destiny)
 syn_packet = TCP(sport=RandShort(), dport=destiny_port, flags="S", seq=1000)
-
 complete_packet = ip_packet / syn_packet
 
-syn_ack_response = sr1(syn_packet, verbose=False, timeout=2, iface="wlp0s20f3")
+syn_ack_response = sr1(complete_packet, verbose=False, timeout=2)
+print()
+print(f"SYN-ACK response | {syn_ack_response}")
 
-if syn_ack_response and syn_ack_response.haslayer(TCP) and syn_ack_response[TCP].flags == "SA":
-    print("SYN-ACK recebido! Conexão estabelecida.")
+if syn_ack_response and syn_ack_response[TCP].flags == "SA":
+    print(f"Correct")
+else: print("No response received")
 
-    #incomplete...
+
+#numero de sequencia retornado pelo servidor 
+my_seq = syn_ack_response[TCP].ack
+my_ack = syn_ack_response[TCP].seq + 1
+ack_source_port = syn_ack_response[TCP].dport
+ack_destiny_port = syn_ack_response[TCP].sport
+ip_ack_packet = IP(dst=syn_ack_response[IP].src)
+
+
+print(f"""
+      
+    Pacote ACK
+    Sequencia: {my_seq}
+    Seq ACK: {my_ack}
+    Porta de origem: {ack_source_port}
+    Porta de destino: {ack_destiny_port}    
+
+""")
+
+ack_packet = TCP(sport=ack_source_port, dport=ack_destiny_port, flags="A", seq=my_seq, ack=my_ack)
+complete_ack_packet = ip_ack_packet / ack_packet
+ack_response = sr1(complete_ack_packet, verbose=False, timeout=2)
+
+#if ack_response
