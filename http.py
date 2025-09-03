fc33b35f39f84daf2c7ef6f63610c591ef7c3890
Mormito <guimormis@gmail.com>
2025-09-03 13:28:07 -0300

    Fix

--- http.py
+++ http.py
@@ -6,7 +6,9 @@ destiny_port = 8080
 http_request = (
     b"GET / HTTP/1.1\r\n"
     b"Host: " + destiny.encode() + b"\r\n"
-    b"User-Agent: Scapy-Client/1.0\r\n"
+    b"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\r\n"
+    b"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
+    b"Accept-Language: en-US,en;q=0.5\r\n"
     b"Connection: close\r\n"
     b"\r\n"
 )
