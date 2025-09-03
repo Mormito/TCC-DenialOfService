19b8e66843ca39fd78c258c297c3be7bdd4c0f76
Mormito <guimormis@gmail.com>
2025-09-03 11:19:26 -0300

    Three-way-handshake

--- http.py
+++ http.py
@@ -1,11 +1,7 @@
-from scapy.all import IP, TCP, Raw, sr1, RandShort, RandIP
+from scapy.all import IP, TCP, Raw, sr1, RandShort, RandIP, send
 
-#def send_http_packet(destiny, destiny_port):
-
-destiny = "google.com" 
-## Como este código ainda não ataca, utilizei sites comuns para enviar requisições HTTP, apenas para testar se estava enviando a requisição.
-## Em nenhum momento causando danos, lentidões ou interrupções a sites.
-destiny_port = 80 ##
+destiny = "8.8.8.8"
+destiny_port = 443
 
 HTTP_REQUEST = (
     b"GET / HTTP/1.1\r\n"
@@ -15,64 +11,36 @@ HTTP_REQUEST = (
     b"\r\n"
 )
 
-#PACOTE SYN COMPLETO (ip,tcp,juncao)
+default_seq = 1000
+
 syn_ip_packet = IP(dst=destiny)
-syn_tcp_packet = TCP(sport=RandShort(), dport=destiny_port, flags="S", seq=1000)
-syn_complete_packet = syn_ip_packet / syn_tcp_packet
+syn_tcp_packet = TCP(sport=23456, dport=destiny_port, flags="S", seq=default_seq)
 
-syn_ack_response = sr1(syn_complete_packet, verbose=False, timeout=2)
-print(f"\nSYN-ACK response | {syn_ack_response}")
+syn_packet = syn_ip_packet / syn_tcp_packet
 
-if syn_ack_response and syn_ack_response[TCP].flags == "SA":
+syn_response = sr1(syn_packet, verbose=False, timeout=2) #synack
+source_port = syn_response[TCP].dport
 
-    #numero de sequencia retornado pelo servidor 
-    my_ack_seq = syn_ack_response[TCP].ack
-    print(f"ACK SEQ: {my_ack_seq}")
-    my_ack_ack = syn_ack_response[TCP].seq + 1
-    print(f"ACK ACK: {my_ack_ack}")
+if syn_response and syn_response[TCP].flags == "SA":
 
-    #PACOTE ACK COMPLETO (ip,tcp,juncao)
-    ack_source_port = syn_ack_response[TCP].dport
-    ack_destiny_port = syn_ack_response[TCP].sport
-    ack_ip_packet = IP(dst=syn_ack_response[IP].src)
-    ack_tcp_packet = TCP(sport=ack_source_port, dport=ack_destiny_port, flags="A", seq=my_ack_seq, ack=my_ack_ack)
+    ack_ip_packet = IP(dst=destiny)
+    ack_tcp_packet = TCP(sport=syn_packet[TCP].sport, dport=destiny_port, flags="A", seq=syn_response[TCP].ack, ack=syn_response[TCP].seq+1 )
+    ack_packet = ack_ip_packet / ack_tcp_packet
+    send(ack_packet, verbose=False)
 
-    ack_complete_packet = ack_ip_packet / ack_tcp_packet
+    print(f"\nSYN Seq: {syn_packet[TCP].seq}")
+    print(f"SYN Ack: {syn_packet[TCP].ack}")
+    print(f"SYN Source Port: {syn_packet[TCP].sport}")
+    print(f"SYN Destiny Port: {syn_packet[TCP].dport}")
 
-    ack_response = sr1(ack_complete_packet, verbose=False)
+    print(f"\nSYN-ACK Seq: {syn_response[TCP].seq}")
+    print(f"SYN-ACK Ack: {syn_response[TCP].ack}")
+    print(f"SYN-ACK Source Port: {syn_response[TCP].sport}")
+    print(f"SYN-ACK Destiny Port: {syn_response[TCP].dport}")
 
-    print(f"""
-        Pacote ACK
-        Sequencia: {my_ack_seq}
-        Seq ACK: {my_ack_ack}
-        Porta de origem: {ack_source_port}
-        Porta de destino: {ack_destiny_port}    
-    """)
-
-    #numero de sequencia retornado pelo servidor 
-    my_http_seq = my_ack_seq
-    my_http_ack = my_ack_ack
-
-    #PACOTE HTTP COMPLETO (ip,tcp,juncao)
-    http_source_port = syn_ack_response[TCP].dport
-    print(http_source_port)
-    http_destiny_port = syn_ack_response[TCP].sport
-    http_ip_packet = IP(dst=ack_response[IP].src)
-    http_tcp_packet = TCP(sport=http_source_port, dport=http_destiny_port, flags="PA", seq=my_http_seq, ack=my_http_ack)
-
-    http_complete_packet = http_ip_packet / http_tcp_packet / Raw(load=HTTP_REQUEST)
-
-    http_response = sr1(http_complete_packet, verbose=False, timeout=3)
-
-    if http_response and http_response.haslayer(Raw):
-        print("\n--- Website content: ---\n")
-        print(http_response[Raw].load.decode(errors='ignore'))
-        print("\n--- END ---\n")
-    else: 
-        print("HTTP Failed")
-        print(f"Server response | {http_response}")
-
-else: print("No response received.")
+    print(f"\nACK Seq: {ack_packet[TCP].seq}")
+    print(f"ACK Ack: {ack_packet[TCP].ack}")
+    print(f"ACK Source Port: {ack_packet[TCP].sport}")
+    print(f"ACK Destiny Port: {ack_packet[TCP].dport}")
 
 
-#simplesmente estou enlouquecendo tentando fazer isso, mas vai dar certo.
