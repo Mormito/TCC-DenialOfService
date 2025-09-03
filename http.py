6873ae50650530f0a821c4583b5e5112ed65b825
Mormito <guimormis@gmail.com>
2025-09-03 13:09:35 -0300

    HTTP Request update / Remake

--- http.py
+++ http.py
@@ -1,9 +1,9 @@
 from scapy.all import IP, TCP, Raw, sr1, RandShort, RandIP, send
 
-destiny = "8.8.8.8"
-destiny_port = 443
+destiny = "10.0.2.5"
+destiny_port = 8080
 
-HTTP_REQUEST = (
+http_request = (
     b"GET / HTTP/1.1\r\n"
     b"Host: " + destiny.encode() + b"\r\n"
     b"User-Agent: Scapy-Client/1.0\r\n"
@@ -14,33 +14,38 @@ HTTP_REQUEST = (
 default_seq = 1000
 
 syn_ip_packet = IP(dst=destiny)
-syn_tcp_packet = TCP(sport=23456, dport=destiny_port, flags="S", seq=default_seq)
+syn_tcp_packet = TCP(sport=RandShort(), dport=destiny_port, flags="S", seq=default_seq)
 
 syn_packet = syn_ip_packet / syn_tcp_packet
 
 syn_response = sr1(syn_packet, verbose=False, timeout=2) #synack
+print(f"\nSYN PACKET SUCEFULLY SENT | FLAG == {syn_packet[TCP].flags}")
 source_port = syn_response[TCP].dport
 
+
 if syn_response and syn_response[TCP].flags == "SA":
+    print(f"SYN-ACK SUCEFULLY RECEIVED | FLAG == {syn_response[TCP].flags}")
 
-    ack_ip_packet = IP(dst=destiny)
-    ack_tcp_packet = TCP(sport=syn_packet[TCP].sport, dport=destiny_port, flags="A", seq=syn_response[TCP].ack, ack=syn_response[TCP].seq+1 )
-    ack_packet = ack_ip_packet / ack_tcp_packet
-    send(ack_packet, verbose=False)
+    http_ip_packet = IP(dst=destiny)
+    http_tcp_packet = TCP(sport=source_port, dport=destiny_port, flags="A", seq=syn_response[TCP].ack, ack=syn_response[TCP].seq+1)
+    http_packet = http_ip_packet / http_tcp_packet / http_request
 
-    print(f"\nSYN Seq: {syn_packet[TCP].seq}")
-    print(f"SYN Ack: {syn_packet[TCP].ack}")
-    print(f"SYN Source Port: {syn_packet[TCP].sport}")
-    print(f"SYN Destiny Port: {syn_packet[TCP].dport}")
+    final_response = sr1(http_packet, verbose=False, timeout=10)
+    
+    if final_response: print(f"ACK PACKET SUCEFFULY SENT | FLAG == {http_packet[TCP].flags}")
 
-    print(f"\nSYN-ACK Seq: {syn_response[TCP].seq}")
-    print(f"SYN-ACK Ack: {syn_response[TCP].ack}")
-    print(f"SYN-ACK Source Port: {syn_response[TCP].sport}")
-    print(f"SYN-ACK Destiny Port: {syn_response[TCP].dport}")
+    if final_response[TCP].flags == "R":
+        print(
+            f"\nSeq and ack (SYN-ACK): {syn_response[TCP].seq} | {syn_response[TCP].ack}"
+            f"\nSeq and ack (HTTP-PACKET): {http_packet[TCP].seq} | {http_packet[TCP].ack}"
+            f"\nSeq and ack: {final_response[TCP].seq} | {final_response[TCP].ack}"
+            f"\nFlags: {final_response[TCP].flags}"
+        )
 
-    print(f"\nACK Seq: {ack_packet[TCP].seq}")
-    print(f"ACK Ack: {ack_packet[TCP].ack}")
-    print(f"ACK Source Port: {ack_packet[TCP].sport}")
-    print(f"ACK Destiny Port: {ack_packet[TCP].dport}")
+    if Raw in final_response:
+        print("\n--- Server Response ---\n")
+        print(final_response[Raw].load.decode())
+    else:
+        print("\nNo raw data in the response. Received flags:", final_response[TCP].flags)
 
 
